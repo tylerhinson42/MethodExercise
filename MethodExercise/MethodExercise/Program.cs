1449dc1d7b3163e8fed13674adfc730f87780760
Tyler Hinson <tyler.hinson42@gmail.com>
2020-01-17 10:49:24 -0600

    First part completed

---
+++ MethodExercise/MethodExercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is your favorite color?");
+            var favoriteColor = Console.ReadLine();
+
+            Console.WriteLine($"Your fav color is {favoriteColor}");
+
+
+            Console.WriteLine("What is your name?");
+            var fullName = Console.ReadLine();
+
+            Console.WriteLine($"Your full name is {fullName}");
+
+
+            Console.WriteLine("What is your favorite animal?");
+            var favoriteAnimal = Console.ReadLine();
+
+            Console.WriteLine($"Your favorite animal is {favoriteAnimal}");
+
+
+            Console.WriteLine("What is your favorite band?");
+            var favoriteBand = Console.ReadLine();
+
+            Console.WriteLine($"Your favorite band is {favoriteBand}");
+
+
+
+        }
+    }
+}
